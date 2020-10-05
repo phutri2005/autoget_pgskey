c4dda9274f40fe9f9a901fd304cd5aa428b6abc3
Tri Pham <phutri2005@yahoo.com.vn>
2020-10-05 20:33:17 +0700

    Update get_keypgs.py
    Refactor main method

--- get_keypgs.py
+++ get_keypgs.py
@@ -11,7 +11,6 @@ import time
 #os.environ['webdriver.chrome.driver'] = 'F:\DevTools\chromedriver_win32_85\chromedriver.exe'
 options = webdriver.ChromeOptions()
 options.add_argument("--start-maximized")
-browser = webdriver.Chrome(executable_path='F:\DevTools\chromedriver_win32_85\chromedriver_hex.exe', options=options)
 
 def checkExistsCheckOut(browser):
     try:
@@ -62,11 +61,16 @@ def clickCheckout(browser):
     browser.minimize_window()
     browser.maximize_window()
 
-    
-#MAIN LOGIN
+def getKey():
+    browser = webdriver.Chrome(executable_path='./chromedriver_hex.exe', options=options)
+    while False == checkExistsCheckOut(browser):
+        time.sleep(1)
+        result = browser.get('https://manage.pgsharp.com/cart.php?a=add&pid=2')
+    clickCheckout(browser)
 
-while False == checkExistsCheckOut(browser):
-    time.sleep(1)
-    result = browser.get('https://manage.pgsharp.com/cart.php?a=add&pid=2')
-    
-clickCheckout(browser)
+
+#Go go go
+getKey()
+
+
+
